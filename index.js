bd9c4f18e3c329473b7ce0a455e81771e42e958c
andyprasetya <andyprasetya@opense7en.com>
2020-04-16 21:29:02 +0700

    Fixing some child-process mechanism

---
+++ index.js
@@ -0,0 +1,8 @@
+var server = require('./lib/server');
+var workers = require('./lib/workers');
+var app = {}; app.init = function(){
+  server.init();
+  workers.init();
+};
+app.init();
+module.exports = app;
