277a66f0bb7997ddde64da2984e38fcab23d122f
Andy Prasetya <andyprasetya@aol.com>
2021-11-20 11:34:30 +0700

    Delete install_service.sh

--- installation/install_service.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo cp trulogix-external-api.service /usr/lib/systemd/system/trulogix-external-api.service && 
-sudo systemctl enable trulogix-external-api.service && 
-echo "Service installation completed."
